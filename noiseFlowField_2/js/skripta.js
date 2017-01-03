c5e47e3a31afdd27dd8c8b860beb6f808d7d536e
Jani Nedeljko <jani.nedeljko@gmail.com>
2017-01-03 20:19:27 +0100

    FlowField

--- noiseFlowField_2/js/skripta.js
+++ noiseFlowField_2/js/skripta.js
@@ -7,12 +7,20 @@ var cols, rows;
 
 var zoff = 0;
 
+var delci = [];
 
+var flowfield;
 
 function setup() {
-createCanvas(600, 600);
+createCanvas(300, 300);
 cols = floor(width / scl);
 rows = floor(height / scl);
+
+flowfield = new Array(cols*rows);
+
+for (var i = 0; i < 300; i++) {
+  delci[i] = new Particle();
+}
 
 
 }
@@ -26,23 +34,32 @@ var yoff = 0;
 for (var y = 0; y < rows; y++) {
   var xoff = 0;
 for (var x = 0; x < cols; x++) {
-      var index = (x+y*width)*4;
+      var index = x+y*cols;
+
       var angle = noise(xoff, yoff,zoff)*TWO_PI;
       var v = p5.Vector.fromAngle(angle);
-
+      v.setMag(5);
+      flowfield[index] = v;
 
             xoff += inc;
 
-            stroke(0);
+            stroke(0,50);
             push();
             translate(x*scl,y*scl);
             rotate(v.heading());
+            strokeWeight(1);
             line(0,0,scl,0);
             pop();
             // rect(x * scl, y * scl, scl, scl);
 }
 yoff += inc;
 zoff += 0.001;
+}
+for (var i = 0; i < delci.length; i++) {
+  delci[i].follow(flowfield),
+  delci[i].update();
+  delci[i].show();
+  delci[i].edges();
 }
 
 
---
+++ noiseFlowField_3/js/skripta.js
@@ -0,0 +1,66 @@
+// var xoff1  = 0;
+// var xoff2  = 10000;
+
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
+
+var zoff = 0;
+
+var delci = [];
+
+var flowfield;
+
+function setup() {
+createCanvas(300, 300);
+cols = floor(width / scl);
+rows = floor(height / scl);
+
+flowfield = new Array(cols*rows);
+
+for (var i = 0; i < 500; i++) {
+  delci[i] = new Particle();
+}
+
+
+}
+
+function draw() {
+  background(255);
+
+
+var yoff = 0;
+
+for (var y = 0; y < rows; y++) {
+  var xoff = 0;
+for (var x = 0; x < cols; x++) {
+      var index = x+y*cols;
+
+      var angle = noise(xoff, yoff,zoff)*TWO_PI*4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+
+            xoff += inc;
+
+            // stroke(0,50);
+            // push();
+            // translate(x*scl,y*scl);
+            // rotate(v.heading());
+            // strokeWeight(1);
+            // line(0,0,scl,0);
+            // pop();
+
+}
+yoff += inc;
+zoff += 0.0003;
+}
+for (var i = 0; i < delci.length; i++) {
+  delci[i].follow(flowfield),
+  delci[i].update();
+  delci[i].show();
+  delci[i].edges();
+}
+
+
+}

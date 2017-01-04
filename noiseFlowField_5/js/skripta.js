893c7b55d372202378eb1a83793325463ae7def3
Jani Nedeljko <jani.nedeljko@gmail.com>
2017-01-04 20:35:33 +0100

    5

---
+++ noiseFlowField_5/js/skripta.js
@@ -0,0 +1,67 @@
+// var xoff1  = 0;
+// var xoff2  = 10000;
+
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
+
+var zoff = 0;
+
+var delci = [];
+
+var flowfield;
+
+function setup() {
+createCanvas(600, 600);
+cols = floor(width / scl);
+rows = floor(height / scl);
+
+flowfield = new Array(cols*rows);
+
+for (var i = 0; i < 1000; i++) {
+  delci[i] = new Particle();
+}
+background(255);
+
+}
+
+function draw() {
+
+
+
+var yoff = 0;
+
+for (var y = 0; y < rows; y++) {
+  var xoff = 0;
+for (var x = 0; x < cols; x++) {
+      var index = x+y*cols;
+
+      var angle = noise(xoff, yoff,zoff)*TWO_PI*4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+
+            xoff += inc;
+
+            // stroke(0,50);
+            // push();
+            // translate(x*scl,y*scl);
+            // rotate(v.heading());
+            // strokeWeight(1);
+            // line(0,0,scl,0);
+            // pop();
+
+}
+yoff += inc;
+zoff += 0.0003;
+}
+for (var i = 0; i < delci.length; i++) {
+  delci[i].follow(flowfield),
+  delci[i].update();
+  delci[i].edges();
+  delci[i].show();
+
+}
+
+
+}
